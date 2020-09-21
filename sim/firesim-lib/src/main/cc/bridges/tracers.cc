1edc048902a8d0616c0239c37bb48554a1331ddf
David Metz <david.metz14@googlemail.com>
2020-09-21 18:18:42 +0200

    fix traceing

--- sim/firesim-lib/src/main/cc/bridges/tracers.cc
+++ sim/firesim-lib/src/main/cc/bridges/tracers.cc
@@ -21,6 +21,7 @@ tracers_t::~tracers_t() {
 
 void tracers_t::init() {
     printf("tracers init\n");
+    write(this->mmio_addrs->traceActive, true);
     write(this->mmio_addrs->initDone, true);
 }
 
@@ -40,7 +41,7 @@ void tracers_t::finish() {
     printf("tracers - li_valid: %x\n", read(this->mmio_addrs->li_valid));
     printf("tracers - li_iaddr: %x\n", read(this->mmio_addrs->li_iaddr));
     printf("tracers - li_insn: %x\n", read(this->mmio_addrs->li_insn));
-    printf("tracers - li_wdata: %x\n", read(this->mmio_addrs->li_wdata));
+//    printf("tracers - li_wdata: %x\n", read(this->mmio_addrs->li_wdata));
     printf("tracers - li_priv: %x\n", read(this->mmio_addrs->li_priv));
     printf("tracers - li_exception: %x\n", read(this->mmio_addrs->li_exception));
     printf("tracers - li_interrupt: %x\n", read(this->mmio_addrs->li_interrupt));
