e7c366076771b53d3d16877dcaf8ee15408cc8ce
David Metz <david.metz14@googlemail.com>
2021-01-13 12:46:39 +0100

    fixed install + added sourceme-lobo.sh

---
+++ sourceme-lobo.sh
@@ -0,0 +1,8 @@
+export XILINXD_LICENSE_FILE=2100@lisens.idi.ntnu.no
+source /opt/convey/pdk2/latest/settings.sh
+source /opt/Xilinx/Vivado/2014.4/settings64.sh
+source scl_source enable devtoolset-8
+source scl_source enable sclo-git25
+source scl_source enable rh-python36
+source ~/git/chipyard/env.sh
+export FIRESIM_ENV_SOURCED=1
